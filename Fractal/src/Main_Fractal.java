cf82ab72648dea004e6aad0c831d057044752652
Eddycas <32119229+Eddycas@users.noreply.github.com>
2017-11-01 17:02:09 -0500

    Add files via upload
    Fractal (Triangulos)

---
+++ Fractal/src/Main_Fractal.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class Main_Fractal extends PApplet {
+
+	public static void main(String[] args) {
+		
+		PApplet.main("Main_Fractal");
+		
+	}
+		
+	public void settings()
+	{
+		size(600, 400);
+	}
+	
+	public void draw()
+	{
+		background(50);
+		
+
+	}
+}
+
